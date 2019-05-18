b5542dc26644a311cc37e6f4e32d46a0a1bee7e4
Ronak Malik <smart10334@gmail.com>
2019-05-18 13:28:22 -0500

    Add Skill serializer

--- Backend/App/serializers.py
+++ Backend/App/serializers.py
@@ -1,22 +1,26 @@
 from rest_framework import serializers
 from .models import Person, Organization, Event, Skill
 
+
 class PersonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Person
-        fields = ('__all__')
+        fields = "__all__"
+
 
 class OrganizationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Organization
-        fields = ('__all__')
+        fields = "__all__"
+
 
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
-        fields = ('__all__')
+        fields = "__all__"
+
 
 class SkillSerializer(serializers.ModelSerializer):
     class Meta:
         model = Skill
-        fields = ('__all__')
+        fields = "__all__"
