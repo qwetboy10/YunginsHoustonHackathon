54872b7edc4c19a2f57c2003e7a322d24cc758ab
Ronak Malik <smart10334@gmail.com>
2019-05-18 13:12:21 -0500

    Setup admin registers

--- Backend/App/admin.py
+++ Backend/App/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Person, Organization, Event
 
+admin.site.register(Person)
+admin.site.register(Organization)
+admin.site.register(Event)
 # Register your models here.
