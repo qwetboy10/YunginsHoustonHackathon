5161c535787211b5e970db8beb9dc92d176f54ad
Meggie Cheng <meggie@seas.upenn.edu>
2019-05-18 14:54:50 -0500

    added Home.js

---
+++ Frontend/front/src/Home.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import './App.css';
+import {getEvents, getOrganizations, getSkills} from './DataFetcher.js';
+import NavBar from './NavBar.js';
+
+class Home extends Component {
+    render() {
+        return (
+          <div>
+            <NavBar />
+          </div>
+        );
+      }
+}
+
+export default Home;
