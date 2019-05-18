2149117ed5864b110d8273d65536e759dec833e6
Meggie Cheng <meggie@seas.upenn.edu>
2019-05-18 15:04:58 -0500

    Added carousel to home.js

--- Frontend/front/src/Home.js
+++ Frontend/front/src/Home.js
@@ -4,12 +4,51 @@ import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import './App.css';
 import {getEvents, getOrganizations, getSkills} from './DataFetcher.js';
 import NavBar from './NavBar.js';
+import {Carousel,  } from 'react-bootstrap';
 
 class Home extends Component {
     render() {
         return (
           <div>
             <NavBar />
+            <Carousel>
+            <Carousel.Item>
+                <img
+                className="d-block w-100"
+                src="holder.js/800x400?text=First slide&bg=373940"
+                alt="First slide"
+                />
+                <Carousel.Caption>
+                <h3>First slide label</h3>
+                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                className="d-block w-100"
+                src="holder.js/800x400?text=Second slide&bg=282c34"
+                alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                <h3>Second slide label</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                className="d-block w-100"
+                src="holder.js/800x400?text=Third slide&bg=20232a"
+                alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                <h3>Third slide label</h3>
+                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            </Carousel>
+
           </div>
         );
       }
