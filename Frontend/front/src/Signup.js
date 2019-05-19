020270980459b95fdccf62d5c1ea7e0aa667b42a
ral <rahul.shanmugha@gmail.com>
2019-05-18 20:46:03 -0500

    Signup.js actually capital now!

--- Frontend/front/src/Signup.js
+++ Frontend/front/src/Signup.js
@@ -25,12 +25,68 @@ class Signup extends Component {
         });
     }
     render() {
-      
         return (
-          <div>
+            <div>
+                <br/>
+                <br/>
+                <MDBContainer>
+                  <MDBRow center>
+                      <MDBCol md="6">
+                      <MDBCard>
+                          <MDBCardBody>
+                          <form>
+                              <p className="h4 text-center py-4">Sign up</p>
+                              <div className="grey-text">
+                              <MDBInput
+                                  label="Your name"
+                                  icon="user"
+                                  group
+                                  type="text"
+                                  validate
+                                  error="wrong"
+                                  success="right"
+                              />
+                              <MDBInput
+                                  label="Your email"
+                                  icon="envelope"
+                                  group
+                                  type="email"
+                                  validate
+                                  error="wrong"
+                                  success="right"
+                              />
+                              
+                              <MDBInput
+                                  label="Your password"
+                                  icon="lock"
+                                  group
+                                  type="password"
+                                  validate
+                              />
+                              <MDBInput
+                                  label="Confirm your password"
+                                  icon="exclamation-triangle"
+                                  group
+                                  type="text"
+                                  validate
+                                  error="wrong"
+                                  success="right"
+                              />
+                              </div>
+                              <div className="text-center py-4 mt-3">
+                              <MDBBtn color="orange" type="submit">
+                                  Register
+                              </MDBBtn>
+                              </div>
+                          </form>
+                          </MDBCardBody>
+                      </MDBCard>
+                      </MDBCol>
+                  </MDBRow>
+              </MDBContainer>
+          </div>
+          );
         
-        </div>
-        );
       
     }
 }
