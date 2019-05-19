91cf1e16e1763cb5bdc244109e808d4d143edcfd
Steven Cheng <stevencheng03@hotmail.com>
2019-05-19 12:05:05 -0500

    fuck ral

--- Frontend/front/src/App.js
+++ Frontend/front/src/App.js
@@ -16,7 +16,6 @@ import CreateEvent from './CreateEvent.js';
 import { getPersonByID } from './DataFetcher.js';
 import OrganizationSignUp from './OrganizationSignUp.js';
 import OrganizationPage from './OrganizationPage.js';
-import CreateEvent from './CreateEvent.js';
 class App extends Component {
   constructor(props) {
     super(props);
