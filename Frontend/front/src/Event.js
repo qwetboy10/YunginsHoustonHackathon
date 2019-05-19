c223bb84180c4021c6b6f085103e8565fe37314b
ral <rahul.shanmugha@gmail.com>
2019-05-19 08:27:15 -0500

    halfway through Event.js:

--- Frontend/front/src/Event.js
+++ Frontend/front/src/Event.js
@@ -3,6 +3,8 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
 import {getEventByID, getPersonByEventID} from './DataFetcher.js';
+import { MDBCard, MDBCardBody, MDBCardImage, MDBCardTitle, MDBCardText, MDBRow, MDBCol, MDBIcon } from 'mdbreact';
+import stockeventpic from './volunteer.jpeg'
 class Event extends Component {
   constructor(props) {
     super(props);
@@ -33,7 +35,24 @@ class Event extends Component {
         return ( //TODO: display info about event and people
           //Create a signup button too and then tell Steven once ur done
           <div>
-                
+            <MDBRow>
+                <MDBCol style={{ maxWidth: "40rem" }}>
+                    <MDBCard reverse>
+                    <MDBCardImage cascade style={{ height: '20rem' }} src="" />
+                    <MDBCardBody cascade className="text-center">
+                        <MDBCardTitle>My adventure</MDBCardTitle>
+                        <h5 className="indigo-text"><strong>Event Name</strong></h5>
+                        <MDBCardText>Full Description</MDBCardText>
+                        <a href="#!" className="icons-sm li-ic ml-1">
+                        <MDBIcon fab icon="linkedin-in" /></a>
+                        <a href="#!" className="icons-sm tw-ic ml-1">
+                        <MDBIcon fab icon="twitter" /></a>
+                        <a href="#!" className="icons-sm fb-ic ml-1">
+                        <MDBIcon fab icon="facebook-f" /></a>
+                    </MDBCardBody>
+                    </MDBCard>
+                </MDBCol>
+            </MDBRow>
         </div>
         );
       
