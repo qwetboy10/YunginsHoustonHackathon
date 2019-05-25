2e8ccb77e5bf62f57121c132d68f9e58196ef71e
Ronak Malik <smart10334@gmail.com>
2019-05-25 10:44:30 -0500

    Changed frontend to read from localhost so it works on others comps

--- Frontend/front/src/DataFetcher.js
+++ Frontend/front/src/DataFetcher.js
@@ -1,4 +1,4 @@
-const djangoIP = 'http://192.168.1.203:8000/api/';
+const djangoIP = 'http://127.0.0.1:8000/api/';
 
 function logError(err) {
     console.log("There was an error when fetching! Exact error below:");
